54f5aaa9930249c178fb13c4ac204393011ae1da
omeryilmaz2K <omeryilmaz2K@gmail.com>
2024-12-30 02:26:04 +0300

    Font Weight parameter added to Base Text Style and generally organized.

--- core/lib/base/text/style/base_text_style.dart
+++ core/lib/base/text/style/base_text_style.dart
@@ -5,30 +5,88 @@ class BaseTextStyle {
   BaseTextStyle._();
 
   static late BuildContext context;
-  static late String? font;
+  static String? _font;
+  static FontWeight? _fontWeight;
 
   static void setContext(BuildContext context) =>
       BaseTextStyle.context = context;
 
-  static setFont(String? font) => BaseTextStyle.font = font;
+  static setFont(String? font) => BaseTextStyle._font = font;
 
-  static displayLarge() => context.displayLarge.copyWith(fontFamily: font);
-  static displayMedium() => context.displayMedium.copyWith(fontFamily: font);
-  static displaySmall() => context.displaySmall.copyWith(fontFamily: font);
+  static setFontWeight(FontWeight weight) => _fontWeight = weight;
 
-  static headlineLarge() => context.headlineLarge.copyWith(fontFamily: font);
-  static headlineMedium() => context.headlineMedium.copyWith(fontFamily: font);
-  static headlineSmall() => context.headlineSmall.copyWith(fontFamily: font);
+  static displayLarge() => context.displayLarge.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
 
-  static titleLarge() => context.titleLarge.copyWith(fontFamily: font);
-  static titleMedium() => context.titleMedium.copyWith(fontFamily: font);
-  static titleSmall() => context.titleSmall.copyWith(fontFamily: font);
+  static displayMedium() => context.displayMedium.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
 
-  static bodyLarge() => context.bodyLarge.copyWith(fontFamily: font);
-  static bodyMedium() => context.bodyMedium.copyWith(fontFamily: font);
-  static bodySmall() => context.bodySmall.copyWith(fontFamily: font);
+  static displaySmall() => context.displaySmall.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
 
-  static labelLarge() => context.labelLarge.copyWith(fontFamily: font);
-  static labelMedium() => context.labelMedium.copyWith(fontFamily: font);
-  static labelSmall() => context.labelSmall.copyWith(fontFamily: font);
+  static headlineLarge() => context.headlineLarge.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
+
+  static headlineMedium() => context.headlineMedium.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
+
+  static headlineSmall() => context.headlineSmall.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
+
+  static titleLarge() => context.titleLarge.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
+
+  static titleMedium() => context.titleMedium.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
+
+  static titleSmall() => context.titleSmall.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
+
+  static bodyLarge() => context.bodyLarge.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
+
+  static bodyMedium() => context.bodyMedium.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
+
+  static bodySmall() => context.bodySmall.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
+
+  static labelLarge() => context.labelLarge.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
+
+  static labelMedium() => context.labelMedium.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
+
+  static labelSmall() => context.labelSmall.copyWith(
+        fontFamily: _font,
+        fontWeight: _fontWeight,
+      );
 }
