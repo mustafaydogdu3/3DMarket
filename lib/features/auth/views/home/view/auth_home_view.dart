9cb4822dad0035307539ea79fc7e8fe00db608be
omeryilmaz2K <omeryilmaz2K@gmail.com>
2025-01-01 21:46:24 +0300

    Register feature completed generally.

--- lib/features/auth/views/home/view/auth_home_view.dart
+++ lib/features/auth/views/home/view/auth_home_view.dart
@@ -16,128 +16,138 @@ class AuthHomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height -
+                MediaQuery.of(context).padding.top -
+                MediaQuery.of(context).padding.bottom,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const SizedBox(height: 48),
-                Wrap(
-                  alignment: WrapAlignment.center,
-                  runSpacing: 24,
+                Column(
                   children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 48),
-                      child: SvgPicture.asset(
-                        AppPaths.loginIll,
-                        height: MediaQuery.of(context).size.height * 0.27,
-                      ),
-                    ),
-                    Text(
-                      AppLocalkeys.loginTitle,
-                      style: BaseTextStyle.headlineMedium(
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 24),
-                Wrap(
-                  runSpacing: 34,
-                  alignment: WrapAlignment.center,
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 12),
-                      child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                              side: const BorderSide(color: Colors.black),
-                            ),
-                            backgroundColor: Colors.white,
+                    const SizedBox(height: 48),
+                    Wrap(
+                      alignment: WrapAlignment.center,
+                      runSpacing: 24,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 48),
+                          child: SvgPicture.asset(
+                            AppPaths.loginIll,
+                            height: MediaQuery.of(context).size.height * 0.27,
                           ),
-                          child: Stack(
-                            alignment: AlignmentDirectional.center,
-                            children: [
-                              SizedBox(
-                                height: 50,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      AppLocalkeys.loginWithGoogle,
-                                      style: BaseTextStyle.bodyLarge(),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Positioned(
-                                left: 0,
-                                child: Brand(
-                                  Brands.google,
-                                  size: 30,
-                                ),
-                              ),
-                            ],
-                          )),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 16),
-                      child: Row(
-                        children: [
-                          const Expanded(child: Divider()),
-                          Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 12),
-                            child: Text(
-                              AppLocalkeys.or,
-                              style: BaseTextStyle.bodyMedium(),
-                            ),
-                          ),
-                          const Expanded(child: Divider()),
-                        ],
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 12),
-                      child: PrimaryButtonWidget(
-                        onPressed: () => showModalBottomSheet(
-                          isScrollControlled: true,
-                          clipBehavior: Clip.antiAliasWithSaveLayer,
-                          context: context,
-                          backgroundColor: Colors.transparent,
-                          builder: (context) => const LoginWithEmailModal(),
                         ),
-                        text: AppLocalkeys.loginWithEmail,
-                      ),
+                        Text(
+                          AppLocalkeys.loginTitle,
+                          style: BaseTextStyle.headlineMedium(
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 24),
+                    Wrap(
+                      runSpacing: 34,
+                      alignment: WrapAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 12),
+                          child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                  side: const BorderSide(color: Colors.black),
+                                ),
+                                backgroundColor: Colors.white,
+                              ),
+                              child: Stack(
+                                alignment: AlignmentDirectional.center,
+                                children: [
+                                  SizedBox(
+                                    height: 50,
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          AppLocalkeys.loginWithGoogle,
+                                          style: BaseTextStyle.bodyLarge(),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Positioned(
+                                    left: 0,
+                                    child: Brand(
+                                      Brands.google,
+                                      size: 30,
+                                    ),
+                                  ),
+                                ],
+                              )),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Row(
+                            children: [
+                              const Expanded(child: Divider()),
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 12),
+                                child: Text(
+                                  AppLocalkeys.or,
+                                  style: BaseTextStyle.bodyMedium(),
+                                ),
+                              ),
+                              const Expanded(child: Divider()),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 12),
+                          child: PrimaryButtonWidget(
+                            onPressed: () => showModalBottomSheet(
+                              isScrollControlled: true,
+                              clipBehavior: Clip.antiAliasWithSaveLayer,
+                              context: context,
+                              backgroundColor: Colors.transparent,
+                              builder: (context) => const LoginWithEmailModal(),
+                            ),
+                            text: AppLocalkeys.loginWithEmail,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      AppLocalkeys.dontHaveAnAccount,
+                      style: BaseTextStyle.bodyMedium(),
+                    ),
+                    TextButton(
+                      onPressed: () => showModalBottomSheet(
+                        isScrollControlled: true,
+                        context: context,
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        useSafeArea: true,
+                        builder: (modalContext) =>
+                            const RegisterWithEmailModal(),
+                      ),
+                      child: Text(
+                        AppLocalkeys.signUp,
+                        style: BaseTextStyle.bodyMedium(),
+                      ),
+                    ),
+                  ],
+                )
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  AppLocalkeys.dontHaveAnAccount,
-                  style: BaseTextStyle.bodyMedium(),
-                ),
-                TextButton(
-                  onPressed: () => showModalBottomSheet(
-                    isScrollControlled: true,
-                    clipBehavior: Clip.antiAliasWithSaveLayer,
-                    context: context,
-                    backgroundColor: Colors.transparent,
-                    builder: (modalContext) => const RegisterWithEmailModal(),
-                  ),
-                  child: Text(
-                    AppLocalkeys.signUp,
-                    style: BaseTextStyle.bodyMedium(),
-                  ),
-                ),
-              ],
-            )
-          ],
+          ),
         ),
       ),
     );
--- lib/features/auth/views/register/modals/register_with_email_modal.dart
+++ lib/features/auth/views/register/modals/register_with_email_modal.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import '../../../../../product/validators/validator.dart';
 import '../../../../../product/values/localkeys/app_localkeys.dart';
 import '../../../../../product/widgets/buttons/primary_button_widget.dart';
+import '../../../../home/view/home_view.dart';
 import '../../../services/auth_service.dart';
 
 class RegisterWithEmailModal extends StatefulWidget {
@@ -26,114 +27,162 @@ class _RegisterWithEmailModalState extends State<RegisterWithEmailModal> {
   final TextEditingController _repeatPasswordController =
       TextEditingController();
 
+  void _showOverlaySnackBar(
+    BuildContext context,
+    String message,
+  ) {
+    final overlay = Overlay.of(context);
+
+    final overlayEntry = OverlayEntry(
+      builder: (context) => Positioned(
+        top: MediaQuery.of(context).padding.top + 16,
+        left: 16,
+        right: 16,
+        child: Material(
+          color: Colors.transparent,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.error,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Text(
+              message,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    overlay.insert(overlayEntry);
+
+    Future.delayed(const Duration(seconds: 3), () {
+      overlayEntry.remove();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(bottom: context.keyboardHeight),
-      height: context.height * 0.45,
-      child: Scaffold(
-        body: Container(
-          padding: const EdgeInsets.all(16),
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.vertical(
-              top: Radius.circular(24),
-            ),
-          ),
-          child: Form(
-            key: _formKey,
-            child: Wrap(
-              runSpacing: 18,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      AppLocalkeys.signupWithEmail,
-                      style: BaseTextStyle.titleLarge(),
-                    ),
-                    IconButton(
-                      onPressed: () => Navigator.pop(context),
-                      icon: const Icon(Icons.close),
-                    ),
-                  ],
-                ),
-                TextFormField(
-                  validator: Validator.email,
-                  keyboardType: TextInputType.emailAddress,
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(8),
-                      ),
-                    ),
-                    labelStyle: BaseTextStyle.bodyMedium(),
-                    labelText: AppLocalkeys.email,
-                    prefixIcon: const Icon(Icons.email),
+      padding: const EdgeInsets.all(16),
+      constraints: BoxConstraints(
+        maxHeight: MediaQuery.of(context).size.height * 0.85,
+      ),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(24),
+        ),
+      ),
+      child: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Wrap(
+            runSpacing: 18,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    AppLocalkeys.signupWithEmail,
+                    style: BaseTextStyle.titleLarge(),
                   ),
-                ),
-                TextFormField(
-                  validator: (value) =>
-                      Validator.password(value, _repeatPasswordController.text),
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(8),
-                      ),
-                    ),
-                    labelStyle: BaseTextStyle.bodyMedium(),
-                    labelText: AppLocalkeys.password,
-                    prefixIcon: const Icon(Icons.lock),
+                  IconButton(
+                    onPressed: () => Navigator.pop(context),
+                    icon: const Icon(Icons.close),
                   ),
-                ),
-                TextFormField(
-                  validator: (value) =>
-                      Validator.repeatPassword(value, _passwordController.text),
-                  controller: _repeatPasswordController,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(8),
-                      ),
+                ],
+              ),
+              TextFormField(
+                validator: Validator.email,
+                keyboardType: TextInputType.emailAddress,
+                controller: _emailController,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8),
                     ),
-                    labelStyle: BaseTextStyle.bodyMedium(),
-                    labelText: AppLocalkeys.confirmPassowrd,
-                    prefixIcon: const Icon(Icons.lock),
                   ),
+                  labelStyle: BaseTextStyle.bodyMedium(),
+                  labelText: AppLocalkeys.email,
+                  prefixIcon: const Icon(Icons.email),
                 ),
-                PrimaryButtonWidget(
-                    onPressed: () async {
-                      FocusScope.of(context).unfocus();
-                      if (_formKey.currentState!.validate()) {
-                        final scaffoldMessenger =
-                            ScaffoldMessenger.of(Navigator.of(context).context);
-                        final String? error = await _authService.register(
-                          _emailController.text,
-                          _passwordController.text,
-                          _repeatPasswordController.text,
-                        );
-                        if (error != null && mounted) {
-                          scaffoldMessenger.showSnackBar(
-                            SnackBar(
-                              content: Text(error),
-                              margin: EdgeInsets.only(
-                                bottom:
-                                    MediaQuery.of(context).viewInsets.bottom +
-                                        16,
-                                left: 16,
-                                right: 16,
-                              ),
-                              behavior: SnackBarBehavior.floating,
-                            ),
-                          );
-                        }
-                      }
-                    },
-                    text: AppLocalkeys.signUp),
-              ],
-            ),
+              ),
+              TextFormField(
+                validator: (value) =>
+                    Validator.password(value, _repeatPasswordController.text),
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                  ),
+                  labelStyle: BaseTextStyle.bodyMedium(),
+                  labelText: AppLocalkeys.password,
+                  prefixIcon: const Icon(Icons.lock),
+                ),
+              ),
+              TextFormField(
+                validator: (value) =>
+                    Validator.repeatPassword(value, _passwordController.text),
+                controller: _repeatPasswordController,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(8),
+                    ),
+                  ),
+                  labelStyle: BaseTextStyle.bodyMedium(),
+                  labelText: AppLocalkeys.confirmPassowrd,
+                  prefixIcon: const Icon(Icons.lock),
+                ),
+              ),
+              PrimaryButtonWidget(
+                onPressed: () async {
+                  FocusScope.of(context).unfocus();
+
+                  if (_formKey.currentState!.validate()) {
+                    showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (context) => const PopScope(
+                        canPop: false,
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      ),
+                    );
+
+                    final String? error = await _authService.register(
+                      _emailController.text,
+                      _passwordController.text,
+                      _repeatPasswordController.text,
+                    );
+
+                    if (!context.mounted) return;
+
+                    Navigator.pop(context);
+
+                    if (error == null) {
+                      Navigator.popUntil(context, (route) => route.isFirst);
+
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const HomeView(),
+                        ),
+                      );
+                    } else {
+                      _showOverlaySnackBar(context, error);
+                    }
+                  }
+                },
+                text: AppLocalkeys.signUp,
+              ),
+            ],
           ),
         ),
       ),
